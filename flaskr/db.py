929a0bbcdb120bae8cdd33603731d4bb8bb5fbdb
Petersburg <pyawinbe@gmail.com>
2023-10-20 08:22:40 +0000

    corrected the code

--- flaskr/db.py
+++ flaskr/db.py
@@ -10,7 +10,7 @@ def get_db():
     """ function to connect to the database """
     if "db" not in g:
         g.db = sqlite3.connect(
-            current_app.config["DATABASE"]
+            current_app.config["DATABASE"],
             detect_types = sqlite3.PARSE_DECLTYPES
         )
 
